087ecc6fa3ad1e8d8afb390c284124cf1704dbb2
Haebin <joker7011@naver.com>
2024-07-09 23:15:27 +0900

    feat: actuator endpoint init

--- src/main/kotlin/com/ggsdh/backend/global/view/HomeController.kt
+++ src/main/kotlin/com/ggsdh/backend/global/view/HomeController.kt
@@ -1,5 +1,6 @@
 package com.ggsdh.backend.global.view
 
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import java.util.*
@@ -9,5 +10,10 @@ class HomeController {
     @GetMapping("/")
     fun getHomePage(locale: Locale): String {
         return "home"
+    }
+
+    @GetMapping("/api/v1/actuator")
+    fun getHealthCheckResponse(): ResponseEntity<Unit> {
+        return ResponseEntity.ok().build()
     }
 }
