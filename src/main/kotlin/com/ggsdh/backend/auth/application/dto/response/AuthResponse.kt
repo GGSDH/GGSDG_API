6b236c7c7e187454999aba04c1614fe18b2a4122
HyungJu <sungkisa@naver.com>
2024-07-15 21:53:59 +0900

    feat: 응답 공통 객체에 감싸자

--- src/main/kotlin/com/ggsdh/backend/auth/application/dto/response/AuthResponse.kt
+++ src/main/kotlin/com/ggsdh/backend/auth/application/dto/response/AuthResponse.kt
@@ -1,7 +1,13 @@
 package com.ggsdh.backend.auth.application.dto.response
 
-data class AuthResponse(
-    val success: Boolean,
-    val member: MemberResponse?,
+class AuthResponse(
     val token: TokenResponse,
-)
+) {
+    companion object {
+        fun of(accessToken: String): AuthResponse {
+            return AuthResponse(
+                TokenResponse(accessToken),
+            )
+        }
+    }
+}
