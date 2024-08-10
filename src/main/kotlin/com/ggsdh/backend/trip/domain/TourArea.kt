1ba2242c3a1136f8cb84b4dc89bb323c5eee5a8d
Haebin <joker7011@naver.com>
2024-08-10 17:07:21 +0900

    feat: DB Schema DATA DDL Init

--- src/main/kotlin/com/ggsdh/backend/trip/domain/TourArea.kt
+++ src/main/kotlin/com/ggsdh/backend/trip/domain/TourArea.kt
@@ -9,8 +9,8 @@ import java.time.LocalDate
 
 @Entity
 @Table(name = "TB_TOUR_AREA")
-@Inheritance(strategy = InheritanceType.JOINED)
-@DiscriminatorColumn(name = "contentType")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "content_type")
 abstract class TourArea(
         @Enumerated(EnumType.STRING)
         var tripThemeConstants: TripThemeConstants,
@@ -27,7 +27,7 @@ abstract class TourArea(
         var mapLevel: Long?,
         var telNo: String?,
         var name: String,
-        var rank: Long?,
+        var ranking: Long?,
 
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
         var dataModifiedAt: LocalDate,
@@ -41,123 +41,119 @@ abstract class TourArea(
     @Column(name = "tour_area_id")
     var id: Long? = null
         private set
-
-    @Entity
-    @Table(name = "TB_TOUR_AREA-TOURISM_SPOT")
-    @DiscriminatorValue("TOURISM_SPOT")
-    class TourismSpotArea(
-            tripThemeConstants: TripThemeConstants,
-            sigunguCode: SigunguCode,
-            contentId: Long,
-            address1: String,
-            address2: String?,
-            image: String?,
-            latitude: Double,
-            longitude: Double,
-            mapLevel: Long?,
-            telNo: String?,
-            name: String,
-            dataModifiedAt: LocalDate,
-            dataCreatedAt: LocalDate,
-            rank: Long?,
-            var grandTourYn: Boolean,
-            var associatedGrandTourYn: Boolean
-    ) : TourArea(
-            tripThemeConstants,
-            sigunguCode,
-            contentId,
-            address1,
-            address2,
-            image,
-            latitude,
-            longitude,
-            mapLevel,
-            telNo,
-            name,
-            rank,
-            dataModifiedAt,
-            dataCreatedAt
-    )
-
-    @Entity
-    @Table(name = "TB_TOUR_AREA-FESTIVAL_EVENT")
-    @DiscriminatorValue("FESTIVAL_EVENT")
-    class FestivalEvent(
-            tripThemeConstants: TripThemeConstants,
-            sigunguCode: SigunguCode,
-            contentId: Long,
-            address1: String,
-            address2: String?,
-            image: String?,
-            latitude: Double,
-            longitude: Double,
-            mapLevel: Long?,
-            telNo: String?,
-            name: String,
-            rank: Long?,
-            dataModifiedAt: LocalDate,
-            dataCreatedAt: LocalDate,
-            var sponsorName: String?,
-            var startDate: LocalDate?,
-            var endDate: LocalDate?,
-            var playTime: String?,
-            var ageLimit: String?,
-            var eventPlace: String?,
-            var spendTimeFestival: String?,
-            var usetimeFestival: String?,
-            var discountInfo: String?
-    ) : TourArea(
-            tripThemeConstants,
-            sigunguCode,
-            contentId,
-            address1,
-            address2,
-            image,
-            latitude,
-            longitude,
-            mapLevel,
-            telNo,
-            name,
-            rank,
-            dataModifiedAt,
-            dataCreatedAt
-    )
-
-    @Entity
-    @Table(name = "TB_TOUR_AREA-RESTAURANT")
-    @DiscriminatorValue("RESTAURANT")
-    class Restaurant(
-            tripThemeConstants: TripThemeConstants,
-            sigunguCode: SigunguCode,
-            contentId: Long,
-            address1: String,
-            address2: String?,
-            image: String?,
-            latitude: Double,
-            longitude: Double,
-            mapLevel: Long?,
-            telNo: String?,
-            name: String,
-            rank: Long?,
-            dataModifiedAt: LocalDate,
-            dataCreatedAt: LocalDate,
-            var firstMenuImage: String?,
-            var firstMenuName: String?
-    ) : TourArea(
-            tripThemeConstants,
-            sigunguCode,
-            contentId,
-            address1,
-            address2,
-            image,
-            latitude,
-            longitude,
-            mapLevel,
-            telNo,
-            name,
-            rank,
-            dataModifiedAt,
-            dataCreatedAt
-    ) {
-    }
 }
+
+@Entity
+@DiscriminatorValue("TOURISM_SPOT")
+class TourismSpotArea(
+        tripThemeConstants: TripThemeConstants,
+        sigunguCode: SigunguCode,
+        contentId: Long,
+        address1: String,
+        address2: String?,
+        image: String?,
+        latitude: Double,
+        longitude: Double,
+        mapLevel: Long?,
+        telNo: String?,
+        name: String,
+        dataModifiedAt: LocalDate,
+        dataCreatedAt: LocalDate,
+        ranking: Long?,
+        var grandTourYn: Boolean,
+        var associatedGrandTourYn: Boolean
+) : TourArea(
+        tripThemeConstants,
+        sigunguCode,
+        contentId,
+        address1,
+        address2,
+        image,
+        latitude,
+        longitude,
+        mapLevel,
+        telNo,
+        name,
+        ranking,
+        dataModifiedAt,
+        dataCreatedAt
+)
+
+@Entity
+@DiscriminatorValue("FESTIVAL_EVENT")
+class FestivalEvent(
+        tripThemeConstants: TripThemeConstants,
+        sigunguCode: SigunguCode,
+        contentId: Long,
+        address1: String,
+        address2: String?,
+        image: String?,
+        latitude: Double,
+        longitude: Double,
+        mapLevel: Long?,
+        telNo: String?,
+        name: String,
+        ranking: Long?,
+        dataModifiedAt: LocalDate,
+        dataCreatedAt: LocalDate,
+        var sponsorName: String?,
+        var startDate: LocalDate?,
+        var endDate: LocalDate?,
+        var playTime: String?,
+        var ageLimit: String?,
+        var eventPlace: String?,
+        var spendTimeFestival: String?,
+        var usetimeFestival: String?,
+        var discountInfo: String?
+) : TourArea(
+        tripThemeConstants,
+        sigunguCode,
+        contentId,
+        address1,
+        address2,
+        image,
+        latitude,
+        longitude,
+        mapLevel,
+        telNo,
+        name,
+        ranking,
+        dataModifiedAt,
+        dataCreatedAt
+)
+
+@Entity
+@DiscriminatorValue("RESTAURANT")
+class Restaurant(
+        tripThemeConstants: TripThemeConstants,
+        sigunguCode: SigunguCode,
+        contentId: Long,
+        address1: String,
+        address2: String?,
+        image: String?,
+        latitude: Double,
+        longitude: Double,
+        mapLevel: Long?,
+        telNo: String?,
+        name: String,
+        ranking: Long?,
+        dataModifiedAt: LocalDate,
+        dataCreatedAt: LocalDate,
+        var firstMenuImage: String?,
+        var firstMenuName: String?
+) : TourArea(
+        tripThemeConstants,
+        sigunguCode,
+        contentId,
+        address1,
+        address2,
+        image,
+        latitude,
+        longitude,
+        mapLevel,
+        telNo,
+        name,
+        ranking,
+        dataModifiedAt,
+        dataCreatedAt
+)
