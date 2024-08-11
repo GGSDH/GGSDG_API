e90ce8f705be5320f868c34e17f12bb601fa52a0
Haebin <joker7011@naver.com>
2024-08-11 18:22:35 +0900

    feat: 노선 조회 최초

--- src/main/kotlin/com/ggsdh/backend/trip/application/LaneService.kt
+++ src/main/kotlin/com/ggsdh/backend/trip/application/LaneService.kt
@@ -1,26 +1,23 @@
 package com.ggsdh.backend.trip.application
 
 import com.ggsdh.backend.global.exception.error.BusinessException
-import com.ggsdh.backend.trip.exception.TripError
+import com.ggsdh.backend.member.exception.MemberError
+import com.ggsdh.backend.member.infrastructure.persistence.MemberRepository
+import com.ggsdh.backend.trip.application.dto.response.LaneResponses
 import com.ggsdh.backend.trip.infrastructure.LaneRepository
 import com.ggsdh.backend.trip.infrastructure.QLaneRepository
 import org.springframework.stereotype.Service
 
 @Service
 class LaneService(
-    private val laneRepository: LaneRepository,
-    private val qLaneRepository: QLaneRepository,
+        private val laneRepository: LaneRepository,
+        private val qLaneRepository: QLaneRepository,
+        private val memberRepository: MemberRepository
 ) {
-    fun test() {
-        val lane = qLaneRepository.test()
-    }
+    fun getThemeLane(id: Long): List<LaneResponses> {
+        val member = memberRepository.findById(id)
+                .orElseThrow { BusinessException(MemberError.NOT_FOUND) }
 
-    fun getThemeLane() {
-        val lane =
-            laneRepository
-                .findById(889L)
-                .orElseThrow { BusinessException(TripError.LANE_NOT_FOUND) }
-
-        return
+        return qLaneRepository.getThemeLanesByMemberId(id)
     }
 }
--- src/main/kotlin/com/ggsdh/backend/trip/infrastructure/QLaneRepository.kt
+++ src/main/kotlin/com/ggsdh/backend/trip/infrastructure/QLaneRepository.kt
@@ -1,22 +1,29 @@
 package com.ggsdh.backend.trip.infrastructure
 
-import com.ggsdh.backend.trip.domain.QLane
+import com.ggsdh.backend.trip.application.dto.response.LaneResponses
+import com.ggsdh.backend.trip.domain.QLane.lane
+import com.ggsdh.backend.trip.domain.QTripTheme.tripTheme
+import com.querydsl.core.types.dsl.Expressions
 import com.querydsl.jpa.impl.JPAQueryFactory
 import org.springframework.stereotype.Repository
 
 @Repository
 class QLaneRepository(
-    private val jpaQueryFactory: JPAQueryFactory,
+        private val query: JPAQueryFactory,
 ) {
-    fun test() {
-        val lanes =
-            jpaQueryFactory
-                .select(
-                    QLane.lane,
-                ).from(
-                    QLane.lane,
-                ).fetch()
+    fun getThemeLanesByMemberId(id: Long): List<LaneResponses> {
+        val lanes = query
+                .select(lane)
+                .from(lane)
+                .leftJoin(tripTheme)
+                .on(tripTheme.tripThemeConstants.eq(lane.tripThemeConstants))
+                .where(tripTheme.member.id.eq(id))
+                .orderBy(Expressions.numberTemplate(Long::class.java, "RAND()").asc())
+                .limit(3)
+                .fetch()
 
-        println(lanes)
+        return lanes.map {
+            LaneResponses(it.id, it.name, it.tripThemeConstants, it.likes)
+        }.toList()
     }
 }
