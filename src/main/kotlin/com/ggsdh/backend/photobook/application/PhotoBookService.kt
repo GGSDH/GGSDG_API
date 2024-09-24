ed16fbda6692cdfbf0d62895b578363a07d3a5de
Jude <lehyungju@gmail.com>
2024-09-24 21:26:33 +0900

    be last

--- src/main/kotlin/com/ggsdh/backend/photobook/application/PhotoBookService.kt
+++ src/main/kotlin/com/ggsdh/backend/photobook/application/PhotoBookService.kt
@@ -96,6 +96,6 @@ class PhotoBookService(
         memberId: Long,
     ): PhotoBook? {
         val photoBooks = photoBookRepository.getAllPhotobookWithoutPhototicket(memberId)
-        return photoBooks.firstOrNull()
+        return photoBooks.lastOrNull()
     }
 }
