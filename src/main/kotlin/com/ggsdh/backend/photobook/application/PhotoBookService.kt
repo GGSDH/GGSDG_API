849bb4b2594fabdd4d1e70d99f1a0c31617ac95e
Jude <lehyungju@gmail.com>
2024-09-25 00:09:18 +0900

    fix

--- src/main/kotlin/com/ggsdh/backend/photobook/application/PhotoBookService.kt
+++ src/main/kotlin/com/ggsdh/backend/photobook/application/PhotoBookService.kt
@@ -58,7 +58,7 @@ class PhotoBookService(
                             dateTime = photo.toGetLocationInputDto().date,
                         ),
                     )
-                }
+                }.join()
             }
         }
 
